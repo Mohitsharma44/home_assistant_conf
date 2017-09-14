5bf196e5317b17ad4b2f1d6c8c90ffd5a6ccce2a
mohitsharma44 <mohitsharma44@gmail.com>
2017-09-14 03:51:52 +0000

    functional version for livingroom sofa

--- clients/mqtt_piswitch/piswitch.py
+++ clients/mqtt_piswitch/piswitch.py
@@ -6,8 +6,7 @@ from hbmqtt.client import MQTTClient, ClientException
 from hbmqtt.mqtt.constants import QOS_0
 
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-
+GPIO.setwarnings(False)
 ##
 # Custom Settings for the Pi's GPIO
 # ----------------------------------
@@ -17,9 +16,16 @@ GPIO.setmode(GPIO.BCM)
 # 26         | Sofa accent lights
 ##
 
-sofa_lights = [6, 19]
-sofa_power  = [13]
-sofa_accent = [26]
+switch_mode = {
+    "on": GPIO.LOW,
+    "off": GPIO.HIGH
+}
+
+gpio_config = {
+    "sofa_lights": [6, 19],
+    "sofa_power": [13],
+    "sofa_accent": [26]
+}
 
 mqtt_host = "hassio.local"
 username  = os.getenv("hassio_username")
@@ -37,16 +43,26 @@ def uptime_coro():
                                                                            mqtt_host=mqtt_host))
     yield from C.subscribe([(topic, QOS_0) for topic in topics])
     try:
-        for i in range(1, 100):
+        while True:
             message = yield from C.deliver_message()
             packet = message.publish_packet
-            print("%d:  %s => %s" % (i,
-                                     packet.variable_header.topic_name,
-                                     str(packet.payload.data)))
-        yield from C.unsubscribe([topic for topic in topics])
-        yield from C.disconnect()
+            topic_name = packet.variable_header.topic_name.split('/')[-1]
+            gpio_channels = gpio_config.get(topic_name)
+            if gpio_channels:
+                print("Got: {} => {}".format(topic_name, gpio_channels))
+                print("%s => %s" % (packet.variable_header.topic_name,
+                                    str(packet.payload.data)))
+                GPIO.output(gpio_channels,
+                            switch_mode.get(packet.payload.data.lower().decode('utf-8')))
+            #yield from C.unsubscribe([topic for topic in topics])
+            #yield from C.disconnect()
     except ClientException as ce:
         logger.error("Client exception: %s" % ce)
         
 if __name__ == '__main__':
+    # Set GPIO board type to BCM a.k.a "Broadcom SOC channel"
+    # (the actual pin numbers next to the GPIO pins)
+    GPIO.setmode(GPIO.BCM)
+    # Set GPIO channel mode to OUT
+    [GPIO.setup(channels, GPIO.OUT) for channels in list(gpio_config.values())]
     asyncio.get_event_loop().run_until_complete(uptime_coro())
